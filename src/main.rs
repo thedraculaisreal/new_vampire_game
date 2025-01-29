0e3aa610580adf33fdfa5addcd848a7f0fada8bf
thedraculaisreal <79720230+thedraculaisreal@users.noreply.github.com>
2025-01-29 08:51:49 -0800

    changing sprite walking frame when direction is switched.

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ struct Player<'a> {
     texture: Texture<'a>,
     sprite: Rect,
     moving: Movement,
+    frame: u32,
 }
 
 #[derive(Default, Clone, Copy)]
@@ -31,31 +32,46 @@ enum Direction {
 }
 
 impl<'a> Player<'a> {
-    pub fn new(pos: Point, texture: Texture<'a>, sprite: Rect, moving: Movement) -> Self {
+    pub fn new(pos: Point, texture: Texture<'a>, sprite: Rect, moving: Movement, frame: u32) -> Self {
 	Self {
 	    pos,
 	    texture,
 	    sprite,
 	    moving,
+	    frame,
 	}
     }
     pub fn change_pos(&mut self) {
-	if self.moving.current {
-	    match self.moving.direction {
-		Direction::Left => {
-		    self.pos = self.pos.offset(-self.moving.speed, 0);
-		},
-		Direction::Right => {
-		    self.pos = self.pos.offset(self.moving.speed, 0);
-		},
-		Direction::Up => {
-		    self.pos = self.pos.offset(0, -self.moving.speed);
-		},
-		Direction::Down => {
-		    self.pos = self.pos.offset(0, self.moving.speed);
-		}
+	match self.moving.direction {
+	    Direction::Left => {
+		self.pos = self.pos.offset(-self.moving.speed, 0);
+	    },
+	    Direction::Right => {
+		self.pos = self.pos.offset(self.moving.speed, 0);
+	    },
+	    Direction::Up => {
+		self.pos = self.pos.offset(0, -self.moving.speed);
+	    },
+	    Direction::Down => {
+		self.pos = self.pos.offset(0, self.moving.speed);
 	    }
 	}
+    }
+    pub fn get_frame(&mut self) -> u32 {
+	match self.moving.direction {
+	    Direction::Left => 1,
+	    Direction::Right => 2,
+	    Direction::Up => 3,
+	    Direction::Down => 0,
+	}   
+    }
+    pub fn change_frame(&mut self) {
+	self.sprite = Rect::new(0,0 + (36 * self.frame) as i32,26, 36);
+    }
+    pub fn update_player(player: &mut Player) {
+	player.change_pos();
+	player.frame = player.get_frame();
+	player.change_frame();
     }
 }
 
@@ -68,20 +84,20 @@ fn main() -> Result<(), String> {
         .build()
         .expect("failed to build window");
     let mut canvas = window.into_canvas().build()
-        .expect("Failed to build canvas");
+        .expect("Failed to build canvas"); 
     let texture_creator = canvas.texture_creator();
-    let texture = texture_creator.load_texture("../assets/reaper.png")?;
-    let texture_1 = texture_creator.load_texture("../assets/reaper.png")?;
-    let texture_2 = texture_creator.load_texture("../assets/bardo.png")?;
+    let texture = texture_creator.load_texture("c:/Users/black/projects/rust/RealVampire/assets/reaper.png")?;
+    let texture_1 = texture_creator.load_texture("c:/Users/black/projects/rust/RealVampire/assets/reaper.png")?;
+    let texture_2 = texture_creator.load_texture("c:/Users/black/projects/rust/RealVampire/assets/bardo.png")?;
     let mut player_list = Vec::new();
     let movement_struct = Movement {
 	direction: Direction::Down,
 	current: false,
 	speed: 5,
     };
-    let mut player = Player::new(Point::new(50,50), texture, Rect::new(0,0,26,36), movement_struct);
-    let player_1 = Player::new(Point::new(50,50), texture_1, Rect::new(0,0,26,36), movement_struct);
-    let player_2 = Player::new(Point::new(75,75), texture_2, Rect::new(0,0,26,36), movement_struct);
+    let mut player = Player::new(Point::new(50,50), texture, Rect::new(0,0,26,36), movement_struct, 1 as u32);
+    let player_1 = Player::new(Point::new(50,50), texture_1, Rect::new(0,0,26,36), movement_struct, 1 as u32);
+    let player_2 = Player::new(Point::new(75,75), texture_2, Rect::new(0,0,26,36), movement_struct, 1 as u32);
     player_list.push(player_1);
     player_list.push(player_2);
     let mut event_pump = sdl_context.event_pump()?;
@@ -119,10 +135,11 @@ fn main() -> Result<(), String> {
 	}
 	// game loop
 	// update function
-	player.change_pos();
+	if player.moving.current {
+	    Player::update_player(&mut player);
+	}
 	// render function
 	render(&mut canvas, Color::RGB(0, 0, 0), &player_list, &player)?;
-
 	// time management
 	std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
